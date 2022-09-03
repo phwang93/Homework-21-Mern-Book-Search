a4504d77ca5623b46ebed2653bdbddf3b547080a
phwang93 <98869145+phwang93@users.noreply.github.com>
2022-09-02 21:35:56 -0700

    fixing issues

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -14,50 +14,46 @@ mutation login($email: String!, $password: String!) {
 `;
 
 export const ADD_USER = gql`
-mutation addUser($username: String!, $email: String!, $password: String!) {
-  addUser(username: $username, email: $email, password: $password) {
-    token
-    user {
+  mutation addUser($username: String!, $email: String!, $password: String!) {
+    addUser(username: $username, email: $email, password: $password) {
+      token
+      user {
+        _id
+        username
+        email
+        bookCount
+        savedBooks {
+          authors
+          bookId
+          image
+          link
+          title
+          description
+        }
+      }
+    }
+  }
+`;
+
+export const SAVE_BOOK = gql`
+  mutation saveBook($book: BookInput!) {
+    saveBook(book: $book) {
       _id
       username
     }
   }
-}
-`;
-
-export const SAVE_BOOK = gql`
-mutation saveBook($bookInput: BookInput!) {
-  saveBook(bookInput: $bookInput) {
-    _id
-    username
-    email
-    savedBooks {
-      bookId
-      authors
-      image
-      description
-      title
-      link
-    }
-  }
-}
 `;
 
 export const REMOVE_BOOK = gql`
-mutation removeBook($bookId: ID!) {
-  removeBook(bookId: $bookId) {
-    _id
-    username
-    email
-    bookCount
-    savedBooks {
-      bookId
-      authors
-      image
-      link
-      title
-      description
+  mutation removeBook($bookId: String!) {
+    removeBook(bookId: $bookId) {
+      savedBooks {
+        authors
+        description
+        bookId
+        image
+        title
+      }
     }
   }
-}
 `;
